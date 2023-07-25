a47fd228d7d5f86587f89499a9ce1ecbdb3cd30e
SushmaJava6366 <96607994+SushmaJava6366@users.noreply.github.com>
2023-07-25 16:50:26 +0530

    Update PowerProgram.java

--- PowerProgram.java
+++ PowerProgram.java
@@ -4,13 +4,11 @@ public class PowerProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int base = 3, power = 3;
+	int base = 3, power = 3;
         int result = 1;
-      
         while (power != 0) {
             result = result * base;
-          
-            power--;
+	    power--;
         }
         System.out.println("Result =  " + result);
 	}
